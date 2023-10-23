e831ec5678008517270539f507707a2e8b2a8c0b
hiroakissh <hiroaki.saito1014@gmail.com>
2023-10-23 22:37:18 +0900

    first commit

---
+++ SampleHapticFeedback/SampleHapticFeedback/SampleHapticFeedbackApp.swift
@@ -0,0 +1,17 @@
+//
+//  SampleHapticFeedbackApp.swift
+//  SampleHapticFeedback
+//
+//  Created by HiroakiSaito on 2023/10/23.
+//
+
+import SwiftUI
+
+@main
+struct SampleHapticFeedbackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
