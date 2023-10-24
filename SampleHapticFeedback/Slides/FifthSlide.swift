ed940c7a1bd72a4c93ac89e59d675dd93abe8927
hiroakissh <hiroaki.saito1014@gmail.com>
2023-10-24 23:14:26 +0900

    [add] add forthSlide and fifthSlide

---
+++ SampleHapticFeedback/Slides/FifthSlide.swift
@@ -0,0 +1,40 @@
+//
+//  FifthSlide.swift
+//  SampleHapticFeedback
+//
+//  Created by HiroakiSaito on 2023/10/24.
+//
+
+import SwiftUI
+import SlideKit
+
+struct FifthSlide: Slide {
+
+    enum SlidePhasedState: Int, PhasedState {
+        case initial, next
+    }
+
+    @Phase var phasedStateStore
+
+    var body: some View {
+        HeaderSlide("自己紹介") {
+            Item("Please tap the right half of this window") {
+                Item("You can go to the next state")
+                Item("You can also use \"return\" or \"→\"")
+            }
+            if phasedStateStore.when(.next) {
+                Item("Please tap the left half of this window") {
+                    Item("You can back the previous slide")
+                    Item("You can also use \"←\"")
+                }
+            }
+        }
+    }
+}
+
+#Preview {
+    SlidePreview {
+        FifthSlide()
+    }
+}
+
