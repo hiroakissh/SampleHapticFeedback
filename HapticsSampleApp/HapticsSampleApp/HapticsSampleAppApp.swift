932912acdaf72e0073adf50088008b7101b2388e
hiroakissh <hiroaki.saito1014@gmail.com>
2023-11-03 18:26:59 +0900

    branch commit

---
+++ HapticsSampleApp/HapticsSampleApp/HapticsSampleAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  HapticsSampleAppApp.swift
+//  HapticsSampleApp
+//
+//  Created by HiroakiSaito on 2023/11/03.
+//
+
+import SwiftUI
+
+@main
+struct HapticsSampleAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
