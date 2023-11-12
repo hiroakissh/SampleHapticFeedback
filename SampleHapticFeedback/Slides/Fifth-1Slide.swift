f66ba0761c93052e5875ca0089e5fd2144429930
hiroakissh <hiroaki.saito1014@gmail.com>
2023-11-13 00:32:31 +0900

    [add] Slideの追加

---
+++ SampleHapticFeedback/Slides/Fifth-1Slide.swift
@@ -0,0 +1,18 @@
+//
+//  Fifth-1Slide.swift
+//  SampleHapticFeedback
+//
+//  Created by HiroakiSaito on 2023/11/13.
+//
+
+import SwiftUI
+
+struct Fifth_1Slide: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Fifth_1Slide()
+}
