2cc5916fb1c05c38fc778f4412fd905502e40369
hiroakissh <hiroaki.saito1014@gmail.com>
2023-11-14 23:09:52 +0900

    [add] add slide

---
+++ SampleHapticFeedback/Slides/Seventh-1Slide.swift
@@ -0,0 +1,18 @@
+//
+//  Seventh-1Slide.swift
+//  SampleHapticFeedback
+//
+//  Created by HiroakiSaito on 2023/11/14.
+//
+
+import SwiftUI
+
+struct Seventh_1Slide: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Seventh_1Slide()
+}
